38d8378afbe7ff04e239dd16c9e003eebe5d24c2
Anvesh Thada <athada@opentext.com>
2019-07-03 10:07:59 +0530

    First Commit

---
+++ src/com/demo/listeners/ExtendedITestListener.java
@@ -0,0 +1,51 @@
+package com.demo.listeners;
+
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+public class ExtendedITestListener implements ITestListener{
+
+	@Override
+	public void onTestStart(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		System.out.println(result.getName()+" Test Case Is Passed");
+		
+	}
+
+	@Override
+	public void onTestFailure(ITestResult result) {
+		System.out.println(result.getName()+" Test Case Is Failed");
+		
+	}
+
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onStart(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onFinish(ITestContext context) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/com/demo/test/SampleTest.java
@@ -0,0 +1,19 @@
+package com.demo.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SampleTest {
+	
+	@Test
+	public void m1() {
+		System.out.println("M1");
+		Assert.assertTrue(true);
+	}
+	
+	@Test
+	public void m2() {
+		System.out.println("M2");
+		Assert.assertTrue(false);
+	}
+}

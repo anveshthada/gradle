8a256b45f54260d90b6da84f64bca802ef6cfdaa
athada <athada@ATHADAPF1HAGFU.opentext.net>
2019-07-04 16:49:43 +0530

    Updated Code

--- src/com/demo/test/SampleTest.java
+++ src/com/demo/test/SampleTest.java
@@ -5,6 +5,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import com.demo.listeners.ExtendedITestListener;
 import com.demo.util.Base;
 
 public class SampleTest extends Base {
@@ -16,6 +17,7 @@ public class SampleTest extends Base {
 	
 	@AfterMethod
 	public void afterMethod() {
+		finished(ExtendedITestListener.testName, ExtendedITestListener.status, ExtendedITestListener.className);
 		driver.quit();
 	}
 	
